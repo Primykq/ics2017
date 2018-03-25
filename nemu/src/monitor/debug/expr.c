018f9946bb4c7b880780f52e270e05a5a87d3e77
tracer-ics2017 <tracer@njuics.org>
2018-03-25 04:22:39 -0400

    >  compile 161620329 primykq Linux debian 4.9.0-4-amd64 #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) x86_64 GNU/Linux  04:22:39 up 1 day, 12:58,  4 users,  load average: 0.00, 0.00, 0.00 b35d65dbc5d6f9171dce48b73b9fb8e14f35b59

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -98,8 +98,6 @@ static bool make_token(char *e) {
 	    tokens[nr_token].type = rules[i].token_type;
 	    strncpy(tokens[nr_token].str,substr_start,substr_len);
 	    tokens[nr_token].str[substr_len] = '\0';
-	    nr_token++;
-	    break;
 	  }
           default: TODO();
         }
