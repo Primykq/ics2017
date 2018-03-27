cf338b89dfa3f6f3702b0a425d0868760eeb7503
tracer-ics2017 <tracer@njuics.org>
2018-03-27 06:09:41 -0400

    >  compile 161620329 primykq Linux debian 4.9.0-4-amd64 #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) x86_64 GNU/Linux  06:09:41 up 2 days,  1:00,  4 users,  load average: 0.00, 0.01, 0.00 41adf133fef92e3b38615eb4c0d1dafb867532

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -263,8 +263,7 @@ uint32_t eval(uint32_t p,uint32_t q){
 	}
 	case TK_RPA:{
 	  if(is_empty(S) == true){
-	    printf("exp is wrong\n");
-	    assert(0);
+	    panic("exp is not the BNF\n");
 	  }
 	  pop(S);
 	  break;
