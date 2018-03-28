216aae0089af3b1d02503891ce910a6923b4fb4f
tracer-ics2017 <tracer@njuics.org>
2018-03-28 07:33:19 -0400

    >  compile 161620329 primykq Linux debian 4.9.0-4-amd64 #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) x86_64 GNU/Linux  07:33:19 up 2 days,  9:44,  4 users,  load average: 0.00, 0.01, 0.02 b97bb5bde4b290c7b61bb6f97f06c45d2373cbb7

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -5,7 +5,8 @@
 
 static WP wp_pool[NR_WP];
 static WP *head, *free_;
-
+WP* new_WP();
+void free_wp(int n);
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
@@ -39,4 +40,40 @@ WP* new_WP(){
   return tempw;
 }
 
-
+void free_wp(int n){
+  WP *temp = head;
+  WP *wp = NULL;
+  while(temp != NULL){
+    if(temp->NO == n){
+      wp = temp;
+      break;
+    }
+    temp = temp->next;
+  }
+  if(wp == NULL){
+    printf("wrong number\n");
+    return ;
+  }
+  if(head == wp){
+    head = wp->next;
+  }
+  else{
+    WP *temp_w = head;
+    while(temp_w->next != wp){
+      temp_w = temp_w->next;
+    }
+    temp_w->next = wp->next;
+    if(free_ == NULL){
+      free_ = wp;
+      wp->next = NULL;
+    }
+    else{
+      WP *temp_f = free_;
+      while(temp_f->next != NULL){
+        temp_f = temp_f->next;
+      }
+      temp_f->next = wp;
+      wp->next = NULL;
+    }
+  }
+}
