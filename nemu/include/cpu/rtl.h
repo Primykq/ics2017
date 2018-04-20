1028b13e7ce4d0cc78276c701898e2d0d6b16b13
tracer-ics2017 <tracer@njuics.org>
2018-04-20 15:02:24 +0800

    >  compile 161620329 primykq Linux Ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:48:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:02:24 up 2 days, 23:02, 14 users,  load average: 0.46, 0.41, 0.50 6cf455ded5b1f9b03a15312c10cc4d6b44fc334b

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -164,7 +164,7 @@ static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-    *dest = ( *src1 == 0 ? 1 : 0 );
+    *dest = ( *src1 == imm ? 1 : 0 );
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
