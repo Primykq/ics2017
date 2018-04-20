77f525549469b1b1a8c54396d53ac41504e54983
tracer-ics2017 <tracer@njuics.org>
2018-04-20 17:37:26 +0800

    >  compile 161620329 primykq Linux Ubuntu 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:48:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:37:26 up 3 days,  1:37, 14 users,  load average: 0.60, 0.55, 0.52 e6d742fca58eb34f160e703c4e4754c1c24b0393

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -151,47 +151,47 @@ void difftest_step(uint32_t eip) {
   // Set `diff` as `true` if they are not the same.
   // TODO();
     if( r.eax != cpu.eax ){
-        printf("r.eax: %x    cpu.eax: %x\n",r.eax,cpu.eax);
+        printf("r.eax: 0x%x    cpu.eax: 0x%x\n",r.eax,cpu.eax);
         diff = true;
     //    return;
     }
     if( r.ecx != cpu.ecx ){
-        printf("r.ecx: %x    cpu.ecx: %x\n",r.ecx,cpu.ecx);
+        printf("r.ecx: 0x%x    cpu.ecx: 0x%x\n",r.ecx,cpu.ecx);
         diff = true;
       //  return;
     }
     if( r.edx != cpu.edx ){
-        printf("r.edx: %x    cpu.edx: %x\n",r.edx,cpu.edx);
+        printf("r.edx: 0x%x    cpu.edx: 0x%x\n",r.edx,cpu.edx);
         diff = true;
     //    return;
     }
     if( r.ebx != cpu.ebx ){
-        printf("r.ebx: %x    cpu.ebx: %x\n",r.ebx,cpu.ebx);
+        printf("r.ebx: 0x%x    cpu.ebx: 0x%x\n",r.ebx,cpu.ebx);
         diff = true;
      //   return;
     }
     if( r.esp != cpu.esp ){
-        printf("r.esp: %x    cpu.esp: %x\n",r.esp,cpu.esp);
+        printf("r.esp: 0x%x    cpu.esp: 0x%x\n",r.esp,cpu.esp);
         diff = true;
     //    return;
     }
     if( r.ebp != cpu.ebp ){
-        printf("r.ebp: %x    cpu.ebp: %x\n",r.ebp,cpu.ebp);
+        printf("r.ebp: 0x%x    cpu.ebp: 0x%x\n",r.ebp,cpu.ebp);
         diff = true;
       //  return;
     }
     if( r.esi != cpu.esi ){
-        printf("r.esi: %x    cpu.esi: %x\n",r.esi,cpu.esi);
+        printf("r.esi: 0x%x    cpu.esi: 0x%x\n",r.esi,cpu.esi);
         diff = true;
      //   return;
     }
     if( r.edi != cpu.edi ){
-        printf("r.edi: %x    cpu.edi: %x\n",r.edi,cpu.edi);
+        printf("r.edi: 0x%x    cpu.edi: 0x%x\n",r.edi,cpu.edi);
         diff = true;
     //    return;
     }
     if( r.eip != cpu.eip ){
-        printf("r.eip: %x    cpu.eip: %x\n",r.eip,cpu.eip);
+        printf("r.eip: 0x%x    cpu.eip: 0x%x\n",r.eip,cpu.eip);
         diff = true;
      //   return;
     }
