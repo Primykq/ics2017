4df925826fee963465003de98ae9eadcb4e22f05
tracer-ics2017 <tracer@njuics.org>
2018-05-02 08:05:14 -0400

    >  run 161620329 primykq Linux debian 4.9.0-4-amd64 #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) x86_64 GNU/Linux  08:05:14 up 1 day,  4:13, 18 users,  load average: 0.00, 0.03, 0.03 944676d856c206abcd7e2a647cce66a1e4372b87

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -3,7 +3,10 @@
 
 #define I8042_STATUS_PORT 0x64
 #define I8042_DATA_PORT 0x60
-#define RTC_PORT 0x48   // Note that this is not standard
+#define RTC_PORT 0x48  
+#define KEYBOARD_IRQ 0x1
+#define I8042_STATUS_HASKEY_MASK 0x1
+// Note that this is not standard
 static unsigned long boot_time;
 
 void _ioe_init() {
